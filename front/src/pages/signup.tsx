49620fb11b603fff101d0239a2f0048d16d8dc6e
nuri <for9595@gmail.com>
2023-11-09 03:29:31 +0900

    scss 미디어쿼리 적용

--- front/src/pages/signup.tsx
+++ front/src/pages/signup.tsx
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
 import routes from "../routes";
 import { useLocation } from "react-router-dom";
-import { InputGroup, Form } from "react-bootstrap";
-
+import { Form, Button } from "react-bootstrap";
+import styled from "styled-components";
+import "../styles/Signup.scss";
 import NavbarLayout from "../components/NavbarLayout";
+
+const FormWrapper = styled(Form)`
+  margin: auto;
+  padding-top: 50px;
+  vertical-align: middle;
+`;
 
 export default function Signup() {
   const location = useLocation(); // 현재 페이지 location
@@ -21,16 +28,33 @@ export default function Signup() {
     <div>
       <NavbarLayout />
 
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
+      <FormWrapper className="Signup">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>이메일 주소</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>닉네임</Form.Label>
+          <Form.Control type="text" />
         </Form.Group>
-      </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>비밀번호</Form.Label>
+          <Form.Control type="password" placeholder="비밀번호" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPasswordcheck">
+          <Form.Label>비밀번호 체크</Form.Label>
+          <Form.Control type="password" placeholder="비밀번호 확인" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check // prettier-ignore
+            type="checkbox"
+            label="가입을 동의합니다."
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          가입하기
+        </Button>
+      </FormWrapper>
     </div>
   );
 }
